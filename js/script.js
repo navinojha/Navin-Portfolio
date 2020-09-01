7a37afdb678b22e711048db5db66fde49b8a479f
singhkaran-123 <navinojha1996@gmail.com>
2020-09-01 17:49:04 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+
+  $('#slides').superslides({
+
+     animation:'fade',
+     play:8000,
+     pagination:false,
+
+  });
+
+
+  var typed = new Typed(".typed", {
+
+  	strings: ["Java Developer.", "Web Developer.", "Software Developer."],
+  	typeSpeed: 70,
+  	loop: true,
+  	startDelay: 1000,
+  	showCursor: false
+
+  });
+
+
+	  $('.owl-carousel').owlCarousel({
+	    loop:true,
+	    items: 4,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        480:{
+	            items:2
+	        },
+	        768:{
+	            items:3
+	        },
+	        938:{
+	            items:4
+	        }
+	    }
+	});
+
+
+
+	var skillsTopOffset = $(".skillsSection").offset().top;
+	var statsTopOffset = $(".statsSection").offset().top;
+	var countUpFinished = false;
+
+	$(window).scroll(function() {
+
+		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+
+			$('.chart').easyPieChart({
+		        easing: 'easeInOut',
+		        barColor: '#fff',
+		        trackColor: false,
+		        scaleColor: false,
+		        lineWidth: 4,
+		        size: 152,
+		        onStep: function(from, to, percent) {
+		        	$(this.el).find('.percent').text(Math.round(percent));
+		        }
+		    });
+			
+
+		}
+
+		if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+  
+             
+			$(".counter").each(function() {
+
+				var element = $(this);
+				var endVal = parseInt(element.text());
+
+				element.countup(endVal);
+
+			})
+
+			countUpFinished = true;
+
+
+		}
+
+
+	});
+
+
+
+});
